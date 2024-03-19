792b71e609d84f4374f615490bfb9c8d30ad82ec
Harshit77Nav <harshit77navneet@gmail.com>
2024-03-19 10:35:16 +0530

    refactored folders

--- backend/src/routes/schedule.route.js
+++ backend/src/routes/schedule.route.js
@@ -1,12 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import getScheduleByDate from "../middlewares/getScheduleByDate.middleware.js";
-import addScheduleMiddleware from '../middlewares/addSchedule.middleware.js';
-import getAllSchedule from '../middlewares/getAllSchedule.middleware.js';
-import updateScheduleByIdMiddleware from '../middlewares/updateSchedule.middleware.js';
-import deleteScheduleByIdMiddleware from '../middlewares/deleteSchedule.middleware.js';
-import addMultipleSchedulesMiddleware from '../middlewares/addMultipleSchedule.middleware.js';
-import deleteMultipleSchedulesByIdMiddleware from '../middlewares/deleteMultipleSchedule.middleware.js';
+import getScheduleByDate from "../Controller/getScheduleByDate.middleware.js";
+import addScheduleMiddleware from '../Controller/addSchedule.middleware.js';
+import getAllSchedule from '../Controller/getAllSchedule.middleware.js';
+import updateScheduleByIdMiddleware from '../Controller/updateSchedule.middleware.js';
+import deleteScheduleByIdMiddleware from '../Controller/deleteSchedule.middleware.js';
+import addMultipleSchedulesMiddleware from '../Controller/addMultipleSchedule.middleware.js';
+import deleteMultipleSchedulesByIdMiddleware from '../Controller/deleteMultipleSchedule.middleware.js';
 
 router.get('/:date', getScheduleByDate, (req, res) => {
   res.json(res.schedule);
